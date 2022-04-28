4fa7f1c0ee277afe4b3f5c027c3a6a0835c1ffd3
Enis <hiherz67@gmail.com>
2022-04-29 00:54:24 +0200

    - Ajout navbar + footer

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class BaseController extends AbstractController
 {
-    #[Route('/base', name: 'app_base')]
+    #[Route('/', name: 'homepage')]
     public function index(): Response
     {
         return $this->render('base/index.html.twig', [
