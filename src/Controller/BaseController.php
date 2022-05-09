b88a1780cdb31f9a60f6b8b1ec88b4c39f1e5959
Enis <hiherz67@gmail.com>
2022-05-09 05:48:12 +0200

    - Modification Navbar et Login - Ajout de plusieurs pages du navbar

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -15,4 +15,33 @@ class BaseController extends AbstractController
             'controller_name' => 'BaseController',
         ]);
     }
+
+    #[Route('/coiffures', name: 'coiffures')]
+    public function coiffures(): Response
+    {
+        return $this->render('pages/coiffures.html.twig', [
+            'controller_name' => 'BaseController',
+        ]);
+    }
+    #[Route('/produits', name: 'produits')]
+    public function produits(): Response
+    {
+        return $this->render('pages/produits.html.twig', [
+            'controller_name' => 'BaseController',
+        ]);
+    }
+    #[Route('/actualites', name: 'actualites')]
+    public function actualites(): Response
+    {
+        return $this->render('pages/actualites.html.twig', [
+            'controller_name' => 'BaseController',
+        ]);
+    }
+    #[Route('/contact', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('pages/contact.html.twig', [
+            'controller_name' => 'BaseController',
+        ]);
+    }
 }
